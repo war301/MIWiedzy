523e7bdca0537e4ae2f8c45777f38c16899469fb
war301 <58977515+war301@users.noreply.github.com>
2022-05-10 18:56:19 +0200

    Update lab4.py

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,89 +1,42 @@
-import random as rand
+import numpy as np 
+import math
+import random as rd 
 
-def montecarlo(foo,foo2, fx, lx, eps):
-    oryginal = foo2(lx)-foo2(fx)
-    wyliczone = 0
-    punkty = 1000
-    ile = 0
-    minimum, maximum = foo(fx), foo(lx)
-    wylosowane=[]
-    ile=0
-    # print(minimum,maximum)
-    while(abs(wyliczone-oryginal)>eps):
-        punkty+=1000
-        for i in range(1000):
-            newx = rand.uniform(fx,lx)
-            newy = rand.uniform(minimum, maximum)
-            while((newx,newy) in wylosowane):
-                newx = rand.uniform(fx,lx)
-                newy = rand.uniform(minimum, maximum)
-            wylosowane.append((newx,newy))
-            
-            wynik = foo(newx)
-            if(newy<=wynik):
-                ile+=1
-        wyliczone = (lx-fx)*(maximum-minimum)*(ile/punkty)
-        print("Wyliczenie niedokładne z dokładnocią: {3}! Dla ilosci {0} punktów:{1}. Rzeczywiste: {2}".format(punkty,wyliczone,oryginal,eps))
-    return wyliczone
+def monte_carlo(function, a, b, ile_punktow):
+    maxValue = max(map(lambda i : function(i), np.linspace(a, b, ile_punktow, True)))
+    points = [(rd.uniform(a,b), rd.uniform(0, maxValue)) for x in range(ile_punktow)] #uniform zwraca losową pomiędzy 
+    print(maxValue)
+    print(rd.uniform(a,b))
+    print(points)
+    lower = upper = 0
+    for x in points:
+        if x[1] < function(x[0]):
+            lower += 1
+        else:
+            upper += 1
+ 
+    return maxValue*(b-a)*(lower/(lower+upper))
 
-def f2(x):
-    return x**2/2
+def riemann(function, a, b, precision):
+    points = tuple(map(lambda i: function(i), np.linspace(a, b, precision, True)))
+    diff = (b-a)/(precision-1)
+ 
+    area = 0
+    for x in points[1:]:
+        area += diff*x
+ 
+    return area
+ 
+def trapmann(function, a, b, precision):
+    points = tuple(map(lambda i: function(i), np.linspace(a, b, precision, True)))
+    diff = (b-a)/(precision-1)
+ 
+    area = 0
+    for x in range(1, precision):
+        area += diff*(points[x]+points[x-1])/2
+ 
+    return area
 
-def f(x):
-    return x
-
-#aproksymacją dolną
-def prostokaty(foo,foo2, fx, lx, eps):
-    oryginal = foo2(lx)-foo2(fx)
-    wyliczone = 0
-    dzielnik = 1
-    odleglosc = 1
-    while(abs(wyliczone-oryginal)>eps):
-        wyliczone = 0
-        dzielnik*=2
-        odleglosc = float(lx-fx)/float(dzielnik)
-        for i in range(dzielnik):
-            wyliczone+= foo(fx+odleglosc*i)*odleglosc
-        print("Wyliczenie niedokładne z dokładnocią: {3}! Dla ilosci {0} prostokatow:{1}. Rzeczywiste: {2}".format(dzielnik,wyliczone,oryginal,eps))
-    return wyliczone
-
-#aproksymacją górną
-def prostokaty2(foo,foo2, fx, lx, eps):
-    oryginal = foo2(lx)-foo2(fx)
-    wyliczone = 0
-    dzielnik = 1
-    odleglosc = 1
-    while(abs(wyliczone-oryginal)>eps):
-        wyliczone = 0
-        dzielnik*=2
-        odleglosc = float(lx-fx)/float(dzielnik)
-        for i in range(1,dzielnik+1):
-            wyliczone+= foo(fx+odleglosc*i)*odleglosc
-        print("Wyliczenie niedokładne z dokładnocią: {3}! Dla ilosci {0} prostokatow:{1}. Rzeczywiste: {2}".format(dzielnik,wyliczone,oryginal,eps))
-    return wyliczone
-
-#aproksymacją srednia
-def prostokaty3(foo,foo2, fx, lx, eps):
-    oryginal = foo2(lx)-foo2(fx)
-    wyliczone = 0
-    dolna = 0
-    gorna = 0
-    dzielnik = 1
-    odleglosc = 1
-    while(abs(wyliczone-oryginal)>eps):
-        wyliczone = 0
-        dzielnik*=2
-        odleglosc = float(lx-fx)/float(dzielnik)
-        for i in range(1,dzielnik+1):
-            dolna = foo(fx+odleglosc*(i-1))*odleglosc
-            gorna = foo(fx+odleglosc*i)*odleglosc
-            wyliczone+= (dolna+gorna)/2
-        print("Wyliczenie niedokładne z dokładnocią: {3}! Dla ilosci {0} prostokatow:{1}. Rzeczywiste: {2}".format(dzielnik,wyliczone,oryginal,eps))
-    return wyliczone
-        
-        
-        
-print(montecarlo(f,f2, 0, 1, 0.05))  ###zakomentowane bo długo trwa (może co nie tak działa???)
-print(prostokaty(f,f2, 0, 1, 0.01))
-print(prostokaty2(f,f2, 0, 1, 0.01))
-print(prostokaty3(f,f2, 0, 1, 0.01))
+#print(monte_carlo(math.sin, 0, math.pi, 100))
+#print(riemann(math.sin, 0, math.pi, 100))
+#print(trapmann(math.sin, 0, math.pi, 100))
