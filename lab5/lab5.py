4d6a8b217a9e6b9008d7fd79d6a978569fb6f256
war301 <58977515+war301@users.noreply.github.com>
2022-05-23 00:34:13 +0200

    Update lab5.py

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -9,21 +9,21 @@ def wczytanie(file):
     return dane 
 
 matrix=wczytanie("australian.dat")
-matrix_2 = [x[:14] for x in matrix] #matrix without decission
+matrix1 = [x[:14] for x in matrix] 
 
-def srednia_aryt_matrix(lista):
+def average(lista):
     ones = np.ones((len(lista),1))
     return float(1/len(lista))*np.dot(np.array(lista),ones)[0]
 
-def wariancja_matrix(lista):
+def covariance(lista):
     srednia = srednia_aryt_matrix(lista)
     ones = np.ones((1,len(lista)))*srednia
     minus = np.array(lista) - ones
     return float(1/len(lista))*np.dot(minus[0],minus[0].T)
 
-def odchylenie_std_matrix(lista):
+def stan_dev(lista):
     return (wariancja_matrix(lista))**(1/2)
 
-print(srednia_aryt_matrix(matrix_2[0]))
-print(wariancja_matrix(matrix_2[0]))
-print(odchylenie_std_matrix(matrix_2[0]))
+print(average(matrix1[0]))
+print(covariance(matrix1[0]))
+print(stan_dev(matrix1[0]))
