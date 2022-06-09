bf65eed58aebc7b9338b911c1723879dc39958df
war301 <58977515+war301@users.noreply.github.com>
2022-06-09 10:48:57 +0200

    Update lab2.py

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -44,11 +44,45 @@ def wczytaj(file):
   return lista      
 
 
-def metryka_euklidesowa(lista,lista1):
+def euklides(lista,lista1):
     suma = 0
     for i in range(len(lista)-1):
         suma += (lista[i] - lista1[i]) ** 2
     return suma ** 0.5
+
+def grupowanie(decyzja,lista):
+    slown ={}
+    for x in range(1,len(lista)):
+        c=lista[x][decyzja]
+        if c in slown:
+            slown[c].append(euklides(lista[0], lista[x]))
+        else:
+            slown[c]=[euklides(lista[0], lista[x])]
+    return slown  
+
+def sortowanie(lista,k):
+    slow = dict()
+    for ele in lista:
+        decyzja = ele[0]
+        if decyzja in slow.keys():
+            slow[decyzja].append(ele[1])
+        else:
+            slow[decyzja]=[ele[1]] 
+    for key in slow.keys():
+        slow[key].sort()
+    for key in slow.keys():
+        suma = 0
+        for ele in slow[key][:k]:
+            suma+= ele
+        slow[key]=suma
+    return slow
+
+def knn_lista(lista,decyzja,nowy):
+    slow = []
+    for x in range(len(lista)):
+        decyzja1 = lista[x][decyzja]
+        slow.append((decyzja1,euklides(nowy, lista[x])))    
+    return slow
 
 def min_odleglosc(slow):
     pom = 1
@@ -63,5 +97,5 @@ def min_odleglosc(slow):
     if pom == 1:        
         return pom1
 
-print(ustalenie_k(grupy))
+
 
